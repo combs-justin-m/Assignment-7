5d1abcf80da24b4a2810f6e3b83bac2ce19e183c
Justin Combs <combs.justin.m@gmail.com>
2015-05-15 14:28:53 -0400

    finished question 5

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-
-// items.forEach(function(item) {
-//   $answer1.appendChild(document.createTextNode(item.price + '\n'))
-// });
-
 console.log(items);
 
 ///////// Question 1 /////////
@@ -37,7 +32,7 @@ var gbp = items.filter(function(item){
   return item.currency_code === "GBP";
 });
 
-document.querySelector('#answer3').textContent = gbp[0].title + ' costs \£' + gbp[0].price;
+document.querySelector('#answer3').textContent = gbp[0].title + ' costs £' + gbp[0].price;
 
 ///////// Question 4 /////////
 var wood = items.filter(function(item){
@@ -53,5 +48,25 @@ wood.forEach(function(list){
 });
 
 ///////// Question 5 /////////
+var eightMats = items.filter(function(item){
+  if (item.materials.length >= 8)
+    return item;
+});
 
-///////// Question 6 /////////
+var $answer5 = document.querySelector('#answer5');
+$answer5.textContent = '';
+
+eightMats.forEach(function(list){
+  $answer5.textContent += '\n' + list.title + ' has ' + list.materials.length + ' materials.' + '\n' + '\n';
+  list.materials.forEach(function(matList){
+    $answer5.textContent += matList + '\n';
+  });
+});
+
+// $answer5.appendChild(document.createTextNode(list.title + '\n'))
+
+
+//   $answer5.textContent += (list.title + ' has ' + list.materials.length + ' materials ' + '\n') +
+//     list.materials;
+// });
+

e66e05789ea7d6560e2a91f72390a42247b1be80
Justin Combs <combs.justin.m@gmail.com>
2015-05-15 10:41:19 -0400

    question 3 complete

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 // items.forEach(function(item) {
-//   $answer1.appendChild(document.createTextNode(item.price + '/n'))
+//   $answer1.appendChild(document.createTextNode(item.price + '\n'))
 // });
 
 console.log(items);
 
-// Question 1 //
+///////// Question 1 /////////
 var allPrices = items.map(function(item){
   return item.price;
 });
@@ -18,6 +18,23 @@ var avg = sum / allPrices.length;
 // Answer //
 var answer1 = avg.toFixed(2);
 
-var $answer1 = document.querySelector('#answer1');
+document.querySelector('#answer1').textContent = '$' + answer1;
 
-$answer1.textContent = '$' + answer1;
+///////// Question 2 /////////
+var costBetween = items.filter(function(item){
+  return (item.price > 14 && item.price < 18);
+})
+
+var $answer2 = document.querySelector('#answer2');
+$answer2.textContent = '';
+
+costBetween.forEach(function(list){
+  $answer2.textContent += (list.title + '\n' + '\n');
+});
+
+///////// Question 3 /////////
+var gbp = items.filter(function(item){
+  return item.currency_code === "GBP";
+});
+
+document.querySelector('#answer3').textContent = gbp[0].title;
